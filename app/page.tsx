70fdd902b3834c30c38ce404f698f082df4b714e
oakdisc <124937047+oakdisc@users.noreply.github.com>
2025-02-16 20:54:06 +0800

    add configuration check on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,19 @@
+"use client";
 import Link from 'next/link';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { LOCAL_STORAGE_KEYS } from './utils/constants';
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const userId = localStorage.getItem(LOCAL_STORAGE_KEYS.USER_ID);
+    if (!userId) {
+      router.push('/welcome');
+    }
+  }, [router]);
+
   return (
     <div>
       <h1>Home Page</h1>
