028f4024d319f5b4fca6dac593122af2986587ab
oakdisc <124937047+oakdisc@users.noreply.github.com>
2025-02-17 01:53:57 +0800

    feat: improve Footer with project and author links

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Box, Container, Typography } from "@mui/material";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import Image from "next/image";
+import Link from "next/link";
 
 const Footer: React.FC = () => {
   return (
@@ -13,15 +14,22 @@ const Footer: React.FC = () => {
         }}
       >
         <Typography variant="body2" color="textSecondary">
-          <Image
-            src="https://img.shields.io/github/stars/oakdisc/gitclone-generator.svg"
-            alt="GitHub Repo stars"
-            width={88}
-            height={20}
-          />
+          <Link
+            href="https://github.com/oakdisc/gitclone-generator"
+            aria-label="Star this project on GitHub"
+          >
+            <Image
+              src="https://img.shields.io/github/stars/oakdisc/gitclone-generator.svg"
+              alt="GitHub Repo stars"
+              width={88}
+              height={20}
+            />
+          </Link>
         </Typography>
         <Typography variant="body2" color="textSecondary">
-          Made with love <FavoriteIcon style={{ height: "1rem" }} /> by oakdisc
+          Made with{" "}
+          <FavoriteIcon style={{ height: "1rem" }} aria-label="love" /> by{" "}
+          <Link href={"https://github.com/oakdisc"}>oakdisc</Link>
         </Typography>
       </Box>
     </Container>
